5afee52c2124ea31b740908ef9ed0fdacfbbb364
Otavi01210 <julio.verga@escola.pr.gov.br>
2026-04-01 07:47:18 -0300

    v.04
    v 04

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let frases = ['F1', 'F2', 'F3', 'F4', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10']
 let frasesCopiada = [...frases]
-
+console.log("oi")
 
 function geradorMensagens() {
    if (frases.length == 0) {
