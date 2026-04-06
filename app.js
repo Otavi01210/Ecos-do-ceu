24c93fe701011777b46b0f1195acce533f875e4b
Otavi01210 <julio.verga@escola.pr.gov.br>
2026-04-06 08:38:30 -0300

    v.05
    v 05

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
-let frases = ['F1', 'F2', 'F3', 'F4', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10']
-let frasesCopiada = [...frases]
-console.log("oi")
-
-function geradorMensagens() {
-   if (frases.length == 0) {
-       frases = [...frasesCopiada]
-   }
-   let indiceGerado = Math.floor(Math.random()*(frases.length));
-   let fraseGerada = frases[indiceGerado];
-   document.querySelector("#msg").textContent = fraseGerada;
-   frases.splice(indiceGerado, 1);
-}
-
-
-geradorMensagens();
-geradorMensagens();
-geradorMensagens();
-geradorMensagens();
-geradorMensagens();
-
+let frases=["F1",
+    "F2",
+    "F3"];
+    
+    function geradorMensagem(){
+    let indiceGerado= Math.floor(Math.random()*(frases.length));
+    let fraseGerada= frases[indiceGerado];
+    document.querySelector("#saída").textContent=fraseGerada;
+    
+    
+    
+    
+    }
+    
