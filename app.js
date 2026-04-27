ef86a2b4e6b16a35ff1778e3a13444a2636143b2
Otavi01210 <julio.verga@escola.pr.gov.br>
2026-04-27 08:44:39 -0300

    v.08
    v 08

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-let frases=["F1",
-    "F2",
-    "F3"];
+let frases=["Os ecos do céu são as respostas que buscamos, sussurrando esperança em cada batida do nosso coração.", "Quando ouvimos os ecos do céu, encontramos a paz que buscamos na Terra.","Que o eco do céu nos conduza, nos fortaleça e nos inspire a viver com propósito e gratidão"];
     
     function geradorMensagem(){
     let indiceGerado= Math.floor(Math.random()*(frases.length));
